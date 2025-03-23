43522639dbfa18cd676a00d189e6f5aa85c6ac60
Rodrigo Verastegui <300095308@fvtc.edu>
2025-03-23 02:22:58 -0500

    Added carousel

--- site.js
+++ site.js
@@ -80,3 +80,46 @@ document.addEventListener("DOMContentLoaded", function () {
     
     localStorage.setItem(key, ' Zelda ');
 });
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+const nextbutton = document.querySelector('#next');
+const previousbutton = document.querySelector('#prev');
+    nextbutton.addEventListener('click', () =>{
+        currentImage = (currentImage +1) % urls.length;
+        showImages();
+});
+    previousbutton.addEventListener('click', () => {
+        currentImage = (currentImage -1 + urls.length) % urls.length;
+        showImages();
+    });
+
+
+    setInterval(() => {
+        // code to run EVERY 5 seconds
+        currentImage = (currentImage + 1) % urls.length;
+        showImages();
+    }, 5000);
+
+    
+    
+
